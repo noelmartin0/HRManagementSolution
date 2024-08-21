1f2b6d92c8b7fe81fe51cca7f5a8b1fbc37a7813
noelmartin0 <noelmartin0@gmail.com>
2024-08-21 15:54:29 +0530

    correction

--- HRManagement/Areas/Admin/Models/PerformanceDetail.cs
+++ HRManagement/Areas/Admin/Models/PerformanceDetail.cs
@@ -2,7 +2,7 @@
 using System.ComponentModel.DataAnnotations.Schema;
 namespace HRManagement.Models
 {
-    [Table("PerdormaceDetails")]
+    [Table("PerformaceDetails")]
     public class PerformanceDetail
     {
        [Key]
