765bc2dd0c8bd4cdcedd0bd8f83d06ce8ff15177
noelmartin0 <noelmartin0@gmail.com>
2024-08-23 15:43:35 +0530

    error fixed

--- HRManagement/Areas/Admin/Models/IEmployeeRepo.cs
+++ HRManagement/Areas/Admin/Models/IEmployeeRepo.cs
@@ -135,6 +135,7 @@
             leaveDetail.EmployeeId = empId;
             leaveDetail.SetDays();
             leaveDetail.CalculateDays();
+            _context.LeaveDetails.Add(leaveDetail);
             _context.SaveChanges();
 
         }
