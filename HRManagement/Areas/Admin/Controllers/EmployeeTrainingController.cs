82a8c908bbd1e8d3c39d30c1f109e7096e6b0e43
Stevic123 <stevicantony@gmail.com>
2024-08-23 18:15:54 +0530

    EmpTrainingController done

---
+++ HRManagement/Areas/Admin/Controllers/EmployeeTrainingController.cs
@@ -0,0 +1,54 @@
+﻿using HRManagement.Areas.Admin.Models;
+using HRManagement.Models;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace HRManagement.Areas.Admin.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeTrainingController : ControllerBase
+    {
+        IEmployeeTrainingRepo _repo;
+        public EmployeeTrainingController(IEmployeeTrainingRepo _repo)
+        {
+            this._repo = _repo;
+        }
+
+        // GET: api/<EmployeeTrainingController>
+        [HttpGet]
+        public List<EmployeeTrainingDetail> Get()
+        {
+            return _repo.GetAllRecords();
+        }
+
+        //// GET api/<EmployeeTrainingController>/5
+        //[HttpGet("{id}")]
+        //public string Get(int id)
+        //{
+        //    return "value";
+        //}
+
+        // POST api/<EmployeeTrainingController>
+        [HttpPost]
+        public void Post([FromBody] EmployeeTrainingDetail value)
+        {
+            _repo.AddEmpTrainingDependancy(value);
+        }
+
+        // PUT api/<EmployeeTrainingController>/5
+        [HttpPut("{id}")]
+        public void Put(int empid,int traid, [FromBody] EmployeeTrainingDetail value)
+        {
+            _repo.UpdateEmpTrainingDependancy(empid, traid, value);
+        }
+
+        // DELETE api/<EmployeeTrainingController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int empid,int traid)
+        {
+            _repo.DeleteEmpTrainingDependancyByEmployeeIdAndTrainingId(empid, traid);
+        }
+    }
+}
---
+++ HRManagement/Areas/Admin/Models/IEmployeeTrainingRepo.cs
@@ -0,0 +1,63 @@
+﻿using HRManagement.Models;
+
+namespace HRManagement.Areas.Admin.Models
+{
+    public interface IEmployeeTrainingRepo
+    {
+        List<EmployeeTrainingDetail> GetAllRecords( );
+        void AddEmpTrainingDependancy(EmployeeTrainingDetail model);
+        void DeleteEmpTrainingDependancyByEmployeeIdAndTrainingId(int empId, int TraId);
+        void UpdateEmpTrainingDependancy(int empId, int TraId, EmployeeTrainingDetail model);
+    }
+
+    public class EmployeeTrainingRepo : IEmployeeTrainingRepo
+    {
+        HRManagementDBContext _context;
+        public EmployeeTrainingRepo(HRManagementDBContext _context)
+        {
+            this._context = _context;
+        }
+        public void AddEmpTrainingDependancy(EmployeeTrainingDetail model)
+        {
+            _context.EmployeeTrainingDetails.Add(model);
+            _context.SaveChanges();
+
+        }
+
+        public void DeleteEmpTrainingDependancyByEmployeeIdAndTrainingId(int empId, int TraId)
+        {
+            EmployeeTrainingDetail model = _context.EmployeeTrainingDetails.FirstOrDefault(et => et.EmployeeId == empId && et.TrainingId == TraId);
+            if (model == null)
+            {
+                throw new InvalidOperationException($"No records found for Employee ID {empId} and Training ID {TraId}");
+            }
+            else
+            {
+                _context.EmployeeTrainingDetails.Remove(model);
+                _context.SaveChanges();
+            }
+        }
+
+        public List<EmployeeTrainingDetail> GetAllRecords()
+        {
+            return _context.EmployeeTrainingDetails.ToList();
+        }
+
+        public void UpdateEmpTrainingDependancy(int empId, int TraId, EmployeeTrainingDetail model)
+        {
+            EmployeeTrainingDetail record = _context.EmployeeTrainingDetails.FirstOrDefault(et => et.EmployeeId == empId && et.TrainingId == TraId);
+            if (record == null)
+            {
+                throw new InvalidOperationException($"No records found for Employee ID {empId} and Training ID {TraId}");
+            }
+            else
+            {
+                record.TrainingStatus = model.TrainingStatus;
+
+            }
+
+        }
+    }
+
+
+}
