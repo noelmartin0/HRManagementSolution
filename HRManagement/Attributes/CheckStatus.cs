3e86b9a9f28653a1ac51080f855923eb9a3bf997
noelmartin0 <noelmartin0@gmail.com>
2024-08-28 17:24:06 +0530

    attribute added

---
+++ HRManagement/Attributes/CheckStatus.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace HRManagement.Attributes
+{
+    public class CheckStatus
+    {
+        public class CheckStatusAttribute : ValidationAttribute
+        {
+            private readonly string[] _statuslist;
+            public CheckStatusAttribute(string[] statuslist)
+            {
+                _statuslist = statuslist;
+            }
+            protected override ValidationResult? IsValid(object value, ValidationContext validationContext)
+            {
+                if (!_statuslist.Contains(value))
+                {
+                    var statusListString = string.Join(", ", _statuslist);
+                    return new ValidationResult($"Status must be one of the following values: {statusListString}.");
+                }
+                return ValidationResult.Success;
+            }
+        }
+    }
+}
